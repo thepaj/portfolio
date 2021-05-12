577eb74d28396cf8d7a61845442d00067ed0982f
thepaj <pavla.jureckova@gmail.com>
2021-05-12 16:32:28 +0200

    game of life added as project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,7 @@ import wyrImage from '../images/wyr.png';
 import portretImage from '../images/portret.jpg';
 import flashcardsImage from '../images/flashcards.jpg';
 import myReadsImage from '../images/myreads.png';
+import GameOfLifeImage from '../images/gameOfLife.png';
 
 class Projects extends React.Component {
     render() {
@@ -14,7 +15,10 @@ class Projects extends React.Component {
                 <div className='page-title'>My projects</div>
                 <div className='projects-container'>
                     <a href="https://laughing-almeida-8be599.netlify.app/" target="_blank" rel="noopener noreferrer">
-                        <Project projectTitle='The Bubble Game' projectDesc='Javascipt | P5.JS' image={bubbleImage}/>
+                        <Project projectTitle='The Bubble Game' projectDesc='Javascipt | P5.js' image={bubbleImage}/>
+                    </a>
+                    <a href="https://eloquent-shaw-b2166b.netlify.app/" target="_blank" rel="noopener noreferrer">
+                        <Project projectTitle='Game Of Life' projectDesc='TypeScript | P5.js' image={GameOfLifeImage}/>
                     </a>
                     <a href="https://5fe2521f48606e00074ef85d--peaceful-montalcini-2888dd.netlify.app/#/" target="_blank" rel="noopener noreferrer">
                         <Project projectTitle='Would-You-Rather Game' projectDesc='React | Redux' image={wyrImage}/>
@@ -26,7 +30,7 @@ class Projects extends React.Component {
                         <Project projectTitle='Flashcards App' projectDesc='React Native | Redux' image={flashcardsImage}/>
                     </a>
                     <a href="https://gallant-hopper-64488a.netlify.app" target="_blank" rel="noopener noreferrer">
-                        <Project projectTitle='Book Inventory Web App' projectDesc='React' image={myReadsImage}/>
+                        <Project projectTitle='Book Web App' projectDesc='React' image={myReadsImage}/>
                     </a>
                 </div>
             </div>
